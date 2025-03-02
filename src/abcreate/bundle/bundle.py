a7e2ddba0865a9eacc283c46cbb456fd791164b3
René de Hesselle <dehesselle@web.de>
2025-03-02 01:56:25 +0100

    Implement Gir

--- src/abcreate/bundle/bundle.py
+++ src/abcreate/bundle/bundle.py
@@ -7,6 +7,7 @@ from pydantic_xml import BaseXmlModel, element, wrapped
 
 from .executable import Executable
 from .framework import Framework
+from .gir import Gir
 from .icon import Icon
 from .library import Library
 from .locale import Locale
@@ -24,6 +25,7 @@ class Bundle(BaseXmlModel, tag="bundle"):
     frameworks: List[Framework] = wrapped(
         "frameworks", element(tag="framework", default_factory=list)
     )
+    gir: Gir
     icon: Icon
     libraries: List[Library] = wrapped(
         "libraries", element(tag="library", default_factory=list)
@@ -63,6 +65,8 @@ class Bundle(BaseXmlModel, tag="bundle"):
 
         for framework in self.frameworks:
             framework.install(bundle_dir, source_dir)
+
+        self.gir.install(bundle_dir, source_dir)
 
         for library in self.libraries:
             library.install(bundle_dir, source_dir)
---
+++ src/abcreate/bundle/gir.py
@@ -0,0 +1,58 @@
+import logging
+from pathlib import Path
+from lxml import etree
+import subprocess
+from tempfile import TemporaryDirectory
+
+from pydantic_xml import BaseXmlModel
+
+log = logging.getLogger("gir")
+
+
+class Gir(BaseXmlModel):
+    dummy: str  # FIXME: this is a workaround
+
+    def install(self, bundle_dir: Path, source_dir: Path):
+        target_dir = bundle_dir / "Contents" / "Resources" / "girepository-1.0"
+        target_dir.mkdir(parents=True, exist_ok=True)
+
+        for source_path in Path(source_dir / "share" / "gir-1.0").glob("*.gir"):
+            target_path = target_dir / source_path.with_suffix(".typelib").name
+            log.info(f"compiling {target_path}")
+
+            tree = etree.parse(source_path)
+            nsmap = {
+                "core": "http://www.gtk.org/introspection/core/1.0",
+                "c": "http://www.gtk.org/introspection/c/1.0",
+                "glib": "http://www.gtk.org/introspection/glib/1.0",
+            }
+            try:
+                element = tree.xpath(
+                    "//core:repository/core:namespace", namespaces=nsmap
+                )[0]
+                libraries = element.attrib["shared-library"].split(",")
+                element.attrib["shared-library"] = ""
+            except KeyError:
+                log.info(f"skipping {target_path}")
+                continue
+
+            for library in libraries:
+                if len(element.attrib["shared-library"]):
+                    element.attrib["shared-library"] += ","
+                element.attrib["shared-library"] += (
+                    Path("@executable_path/../Frameworks") / Path(library).name
+                ).as_posix()
+
+            with TemporaryDirectory() as temp_dir:
+                gir_file = Path(temp_dir) / source_path.name
+                tree.write(gir_file, pretty_print=True)
+                subprocess.run(
+                    [
+                        f"{source_dir}/usr/bin/jhb",
+                        "run",
+                        "g-ir-compiler",
+                        "-o",
+                        target_path,
+                        gir_file,
+                    ]
+                )
