7158196ba0fba0a717e7a5a23e9862fc140b8739
René de Hesselle <dehesselle@web.de>
2026-05-09 17:38:31 +0200

    Move cli setup into cli module

--- src/abcreate/__init__.py
+++ src/abcreate/__init__.py
@@ -4,60 +4,25 @@
 
 import argparse
 import logging
-from enum import Enum
 from pathlib import Path
 
-from abcreate.bundle import Bundle
+from abcreate.util.cli import setup_cli
 from abcreate.util.log import logstats, setup_logging
-from abcreate.util.version import VERSION
 
 log = logging.getLogger("main")
 
 
-class Command(Enum):
-    CREATE = "create"
-
-
 def main() -> None:
     parser = argparse.ArgumentParser(description="create an application bundle")
-    parser.add_argument("--version", action="version", version=f"abcreate {VERSION}")
-    p_commands = parser.add_subparsers(help="available commands", dest="command")
-
-    p_create = p_commands.add_parser(
-        Command.CREATE.value, help="create application bundle"
-    )
-    p_create.add_argument("file", type=Path, help="XML configuration file")
-    p_create.add_argument(
-        "-i",
-        "--install_prefix",
-        type=Path,
-        required=True,
-        help="install prefix of the application",
-    )
-    p_create.add_argument(
-        "-o",
-        "--output_dir",
-        type=Path,
-        required=True,
-        help="directory to create the .app bundle in",
-    )
-
+    setup_cli(parser)
     args = parser.parse_args()
+    setup_logging(Path("abcreate.log"))
 
-    if args.command == Command.CREATE.value:
-        setup_logging(Path("abcreate.log"))
-        log.info(f"abcreate {VERSION}")
-
-        try:
-            xml_doc = args.file.read_text()
-            bundle = Bundle.from_xml(xml_doc)
-        except Exception as e:
-            log.critical(e)
-        else:
-            bundle.create(args.output_dir, args.install_prefix)
-
-    else:
+    try:
+        args.func(args)
+    except AttributeError:
         parser.print_usage()
+        exit(1)
 
     log.info(f"finished with {logstats.warnings} warnings and {logstats.errors} errors")
 
---
+++ src/abcreate/util/cli.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: 2026 René de Hesselle <dehesselle@web.de>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+import argparse
+import logging
+from enum import Enum
+from pathlib import Path
+
+from abcreate.bundle import Bundle
+from abcreate.util.version import VERSION
+
+log = logging.getLogger("main")
+
+
+class Command(Enum):
+    CREATE = "create"
+
+
+def create_app(args: argparse.Namespace) -> None:
+    log.info(f"abcreate {VERSION}")
+
+    try:
+        xml_doc = args.file.read_text()
+        bundle = Bundle.from_xml(xml_doc)
+    except Exception as e:
+        log.critical(e)
+    else:
+        bundle.create(args.output_dir, args.install_prefix)
+
+
+def setup_cli(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument("--version", action="version", version=f"abcreate {VERSION}")
+    commands = parser.add_subparsers(help="available commands", dest="command")
+
+    create_command = commands.add_parser(
+        Command.CREATE.value, help="create application bundle"
+    )
+    create_command.add_argument("file", type=Path, help="XML configuration file")
+    create_command.add_argument(
+        "-i",
+        "--install_prefix",
+        type=Path,
+        required=True,
+        help="install prefix of the application",
+    )
+    create_command.add_argument(
+        "-o",
+        "--output_dir",
+        type=Path,
+        required=True,
+        help="directory to create the .app bundle in",
+    )
+    create_command.set_defaults(func=create_app)
